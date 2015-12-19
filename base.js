e954807a57f96d5b00995454cd3cc021fe28d029
reus <reusee@gmail.com>
2015-12-19 12:15:47 +0800

    clean-ups

--- base.js
+++ base.js
@@ -3,6 +3,8 @@ import diff from 'virtual-dom/diff'
 import patch from 'virtual-dom/patch'
 import createElement from 'virtual-dom/create-element'
 
+let debug = true;
+
 class Thunk {
   constructor(renderFn, state, shouldUpdate) {
     this.type = 'Thunk';
@@ -13,7 +15,7 @@ class Thunk {
 
   render(previous) {
     var previousState = previous ? previous.state : null;
-    if (this.shouldUpdate(previousState)) {
+    if (this.shouldUpdate(this.state, previousState)) {
       return this.renderFn(this.state);
     }
     return previous.vnode;
@@ -22,68 +24,71 @@ class Thunk {
 
 export class Component {
   constructor(state) {
-    this.state = state;
-    this.name = this.constructor.name;
-    this.thunk = this.newThunk();
+    this.thunk = this.newThunk(state);
   }
 
-  newThunk() {
+  newThunk(state) {
     return new Thunk((state) => {
       let Hook = function(){}
-      Hook.prototype.hook = this.afterRender.bind(this);
-      let vnode = h(this.name, {
-        'after-render': new Hook(),
+      Hook.prototype.hook = (node) => {
+        this.node = node;
+        this.nodeChanged(node);
+      };
+      let vnode = h(this.constructor.name, {
+        'node-changed': new Hook(),
       }, [this.render(state)]);
       return vnode;
-    }, this.state, this.shouldUpdate);
+    }, state, this.shouldUpdate);
   }
 
   render(state) {
     return h();
   }
 
-  bind(root) {
+  bind(parent) {
     this.node = createElement(this.thunk);
-    root.appendChild(this.node);
+    parent.appendChild(this.node);
   }
 
   set(newState) {
-    this.state = newState;
-    let newThunk = this.newThunk();
+    let newThunk = this.newThunk(newState);
     let patches = diff(this.thunk, newThunk);
     this.node = patch(this.node, patches);
     this.thunk = newThunk;
   }
 
-  afterRender(node, propertyName, previousState) {
+  nodeChanged(node) {
+    if (debug) {
+      console.log('node changed', this.constructor.name);
+    }
   }
 
-  shouldUpdate(previousState) {
-    if (!previousState || !this.state) {
+  shouldUpdate(state, previousState) {
+    if (!previousState || !state) {
       return true;
     }
     let previousType = typeof previousState;
-    let currentType = typeof this.state;
+    let currentType = typeof state;
     if (previousType != currentType) {
       return true;
     }
     switch (currentType) {
     case 'object': // shallow check
       let previousKeys = Object.keys(previousState);
-      let currentKeys = Object.keys(this.state);
+      let currentKeys = Object.keys(state);
       if (previousKeys.length != currentKeys.length) {
         return true;
       }
       let len = currentKeys.length;
       for (let i = 0; i < len; i++) {
         let key = currentKeys[i];
-        if (previousState[key] !== this.state[key]) {
+        if (previousState[key] !== state[key]) {
           return true;
         }
       }
       return false;
     default:
-      return !(previousState === this.state);
+      return !(previousState === state);
     }
   }
 }

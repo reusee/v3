e66988a6301a84bf31d06627b0b82822314c6863
reus <reusee@gmail.com>
2016-03-21 17:08:46 +0800

    fix golang api server

--- golang/call.js
+++ golang/call.js
@@ -8,7 +8,6 @@ export function call(what, data, cb, retry = 5) {
     url: apiEndpoint + '/' + what,
     type: 'json',
     method: 'post',
-    withCredentials: true,
     data: JSON.stringify(data),
     success: (resp) => {
       cb(resp);
